2a6b0b7a10145ed0e73b87d7ffd0dd4a197e75ac
write2cmdg <write2cmdg@gmail.com>
2023-02-09 19:47:22 -0500

    test animation

--- src/components/AnimatedBackground.js
+++ src/components/AnimatedBackground.js
@@ -7,11 +7,11 @@ const AnimatedBackground = () => {
     const interval = setInterval(() => {
       const x = Math.floor(Math.random() * 50);
       const y = Math.floor(Math.random() * 50);
-      const size = Math.floor(Math.random() * 500 + 50);
+      const size = Math.floor(Math.random() * 250 + 50);
       const color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
 
       setCircles([...circles, { x, y, size, color }]);
-    }, 250);
+    }, 500);
 
     return () => clearInterval(interval);
   }, [circles]);
