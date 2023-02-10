054cc680d82c5ef93e3f1b5b8a29933dc507b0aa
write2cmdg <write2cmdg@gmail.com>
2023-02-09 21:35:01 -0500

    fixes adds nan response

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  let [total, setTotal] = useState(0.00);
+  let [total, setTotal] = useState();
   let [points, setPoints] = useState(6.375);
   let [hostOneHours, setHostOneHours] = useState(6);
   let [hostTwoHours, setHostTwoHours] = useState(4);
@@ -32,7 +32,7 @@ function App() {
       <h2>tips info:</h2>
           <div className='form-group'>
             <h4>Total tips: </h4>
-            <input  type="text" name="total" value={total} placeholder={total} onChange={(e) => setTotal(e.target.value)} />
+            <input  type="text" name="total" value={total} placeholder={'$0.00'} onChange={(e) => setTotal(e.target.value)} />
           </div>
           <div className='form-group'>
             <h4>Total points: </h4>
@@ -65,27 +65,27 @@ function App() {
       <h2>tipout totals:</h2>
 
         <div className="result-line">
-          <h4>Host 1 takes </h4><span className="result">${tipsHostOne.toFixed(0)}</span>
+          <h4>Host 1 takes </h4><span className="result">${!tipsHostOne ? '0' : tipsHostOne.toFixed(0)}</span>
         </div>
         <div className="result-line">
-          <h4>Host 2 takes </h4><span className="result">${tipsHostTwo.toFixed(0)}</span>
+          <h4>Host 2 takes </h4><span className="result">${!tipsHostTwo ? '0' : tipsHostTwo.toFixed(0)}</span>
         </div>
         <div className="result-line">
-          <h4>Host 3 takes </h4><span className="result">${tipsHostThree.toFixed(0)}</span>
+          <h4>Host 3 takes </h4><span className="result">${!tipsHostThree ? '0' : tipsHostThree.toFixed(0)}</span>
         </div>
         <br />
         <hr /> <br />
         <div className="result-line">
-          <h4>Full point takes: </h4> <span className="result">${full.toFixed(0)}</span>
+          <h4>Full point takes: </h4> <span className="result">${!full ? '0' : full.toFixed(0)}</span>
         </div>
         <div className="result-line">
-          <h4>0.5 point takes: </h4> <span className='result'>${p5.toFixed(0)}</span>
+          <h4>0.5 point takes: </h4> <span className='result'>${!p5 ? '0' : p5.toFixed(0)}</span>
         </div>
         <div className="result-line">
-          <h4>0.75 point takes: </h4> <span className='result'>${p75.toFixed(0)}</span>
+          <h4>0.75 point takes: </h4> <span className='result'>${!p75 ? '0' : p75.toFixed(0)}</span>
         </div>
         <div className="result-line">
-          <h4>0.375 point takes: </h4> <span className='result'>${p375.toFixed(0)}</span>
+          <h4>0.375 point takes: </h4> <span className='result'>${!p375 ? '0' : p375.toFixed(0)}</span>
         </div>
       <div className="small">If you had a great shift and want to buy me a coffee click <a href="https://account.venmo.com/u/Cristian-DiGrandi">HERE</a>. Thanks!!</div>
       </div>
